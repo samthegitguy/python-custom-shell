ddd057aef4de4fddfa955208b4954e232698b431
samthegitguy <pokemonexpok@gmail.com>
2020-03-05 01:46:28 +0000

    BETA
    Working but with low data - will update data directory soon

--- shell.py
+++ shell.py
@@ -1,1 +1,23 @@
-#Welcome To The File
+print("Initiating boot sequence")
+print("Saving data")
+
+knowncommands = ["test","exit"]
+def commandnum(enteredcommand, thelistofknowncommands):
+    if enteredcommand in thelistofknowncommands:
+        loop = True
+        x = 0
+        while loop == True:
+            if enteredcommand == thelistofknowncommands[x]:
+                loop = False
+                return x
+            else:
+                x = x + 1
+exit = False
+while exit != True:
+    cmd = commandnum(raw_input("Enter your command: "), knowncommands)
+    if cmd == 0:
+        print("Testing was successful")
+    elif cmd == 1:
+        exit = True
+print("Goodbye!")
+
